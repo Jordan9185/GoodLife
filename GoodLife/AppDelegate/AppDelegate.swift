d20a4e3e2faf42cf89f9fa5425cf578186feff6a
jordan9185 <jordan5011@yahoo.com>
2017-09-27 16:02:46 +0800

    First Commit.

---
+++ GoodLife/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GoodLife
+//
+//  Created by JordanLin on 2017/9/27.
+//  Copyright © 2017年 JordanLin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+       
+        return true
+    }
+
+}
+
